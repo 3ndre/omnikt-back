c7049bbc30b643436c1ab860af05d5935d11bb91
3ndless_____ <57608074+3ndre@users.noreply.github.com>
2023-02-18 12:36:09 +0545

    Added centralized data update endpoints

--- routes/api/contract.js
+++ routes/api/contract.js
@@ -40,7 +40,7 @@ router.get("/", authenticateToken, async (req, res) => {
 router.get('/:contractId', async (req, res) => {
  
   try {
-    const contract = await Contract.findById(req.params.contractId).select('-url').select('-_id').select('-wallet');
+    const contract = await Contract.findById(req.params.contractId).select('-_id').select('-wallet');
     if(contract !== null) {
       res.json(contract);
     } else {
@@ -134,6 +134,91 @@ router.post(
     }
   }
 );
+
+
+//---------------------------------------------------------------------------------------------------------------------
+//Updating a contractContent for nft
+
+router.put(
+  '/update/:id', authenticateToken,
+  async (req, res) => {
+
+    const { wallet, name, symbol, amount, price, url, type, contractAddress, contentImageUrl, chains, trusted, mint } = req.body;
+    const { id } = req.params;
+
+    const opts = {
+      overwrite: true,
+      invalidate: true,
+      resource_type: "auto",
+    }
+
+    try {
+
+      let contractContent = await Contract.findById(id);
+
+      if (!contractContent) {
+        return res.status(404).json({ msg: 'Contract not found' });
+      }
+
+      if(contentImageUrl !== null) {
+        let contentImageUpload = await cloudinary.uploader.upload(contentImageUrl, opts, (err, result) => {
+          if(result && result.secure_url) {
+            const cloudinaryUrl = result.secure_url;
+            return cloudinaryUrl
+          } else {
+          console.log(err.message);
+          return null
+        }
+        })
+
+        let contentImage = contentImageUpload.secure_url
+
+        contractContent.wallet = wallet;
+        contractContent.name = name;
+        contractContent.symbol = symbol;
+        contractContent.contentImage = contentImage;
+        contractContent.amount = amount;
+        contractContent.price = price;
+        contractContent.url = url;
+        contractContent.type = type;
+        contractContent.contractAddress = contractAddress;
+        contractContent.chains = chains;
+        contractContent.trusted = trusted;
+        contractContent.mint = mint;
+
+        await contractContent.save();
+        res.json(contractContent);
+
+      } else {
+
+        let contentImage = contentImageUrl;
+
+        contractContent.wallet = wallet;
+        contractContent.name = name;
+        contractContent.symbol = symbol;
+        contractContent.contentImage = contentImage;
+        contractContent.amount = amount;
+        contractContent.price = price;
+        contractContent.url = url;
+        contractContent.type = type;
+        contractContent.contractAddress = contractAddress;
+        contractContent.chains = chains;
+        contractContent.trusted = trusted;
+        contractContent.mint = mint;
+
+        await contractContent.save();
+        res.json(contractContent);
+
+      }
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Unable to update the contract!');
+    }
+  }
+);
+
+
 
 //---------------------------------------------------------------------------------------------------------------------
 
